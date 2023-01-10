bc82d0f8d87bef5133ec6a958bae98b527960c88
Edw4rd0 <e.gaudiosi@studenti.unina.it>
2023-01-10 12:07:10 +0100

    User login implementation

--- src/main/java/me/csaprotocol/tortoisehospital/HelloApplication.java
+++ src/main/java/me/csaprotocol/tortoisehospital/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginUser.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 880, 500);
         stage.setTitle("TortoiseHospital");
         stage.setScene(scene);
         stage.show();
